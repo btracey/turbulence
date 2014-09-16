ab7d36382f26606a7404488d53b8b2c00b444612
btracey <bdtinternet@gmail.com>
2014-09-15 18:45:06 -0700

    Fixed problems with SA computation. Now reflects SU2

--- sa/spalart_allmaras.go
+++ sa/spalart_allmaras.go
@@ -7,6 +7,7 @@ const (
 	Sigma               = 2.0 / 3.0
 	CB2                 = 0.622
 	Kappa               = 0.41
+	K2                  = Kappa * Kappa
 	CW2                 = 0.3
 	CW3                 = 2
 	CV1                 = 7.1
@@ -31,13 +32,14 @@ type SA struct {
 	LimitedWallDistance float64
 
 	// Intermediate quantities
-	W     [][]float64
-	Chi   float64
-	Ft2   float64
-	Omega float64
-	SHat  float64
-	Fv1   float64
-	Fv2   float64
+	W       [][]float64
+	Chi     float64
+	Ft2     float64
+	Omega   float64
+	SHat    float64
+	InvSHat float64
+	Fv1     float64
+	Fv2     float64
 
 	Production      float64
 	Destruction     float64
@@ -69,10 +71,9 @@ func (s *SA) computeFv2() {
 func (s *SA) computeSHat() {
 	s.computeOmega()
 	s.computeFv2()
-	s.SHat = s.Omega + (s.NuHat/(Kappa*Kappa*s.WallDistance*s.WallDistance))*s.Fv2
-	//if s.SHat < 1e-8 {
-	//	s.SHat = 1e-8
-	//}
+	d2 := s.WallDistance * s.LimitedWallDistance
+	invk2d2 := 1.0 / (K2 * d2)
+	s.SHat = s.Omega + s.NuHat*s.Fv2*invk2d2
 }
 
 func (s *SA) computeOmega() {
@@ -94,7 +95,7 @@ func (s *SA) computeOmega() {
 }
 
 func (s *SA) computeR() {
-	r := s.NuHat / (s.SHat * Kappa * Kappa * s.WallDistance * s.WallDistance)
+	r := s.NuHat * s.InvSHat / (Kappa * Kappa * s.LimitedWallDistance * s.LimitedWallDistance)
 	if r > 10 {
 		r = 10
 	}
@@ -113,11 +114,13 @@ func (s *SA) computeFw() {
 }
 
 func (s *SA) computeProduction() {
-	s.Production = CB1 * (1 - s.Ft2) * s.SHat * s.NuHat
+	//s.Production = CB1 * (1 - s.Ft2) * s.SHat * s.NuHat
+	s.Production = CB1 * s.SHat * s.NuHat
 }
 
 func (s *SA) computeDestruction() {
-	s.Production = (CW1*s.Fw - Cb1OverKappaSquared*s.Ft2) * (s.NuHat / s.WallDistance) * (s.NuHat / s.WallDistance)
+	//s.Destruction = (CW1*s.Fw - Cb1OverKappaSquared*s.Ft2) * (s.NuHat / s.WallDistance) * (s.NuHat / s.WallDistance)
+	s.Destruction = CW1 * s.Fw * (s.NuHat / s.LimitedWallDistance) * (s.NuHat / s.LimitedWallDistance)
 }
 
 func (s *SA) computeCrossProduction() {
@@ -129,20 +132,37 @@ func (s *SA) computeCrossProduction() {
 
 // Compute all of the values
 func (s *SA) Source() float64 {
+	if s.WallDistance == 0 {
+		return 0
+	}
 	s.LimitedWallDistance = s.WallDistance
 	if !s.Unlimited {
 		if s.LimitedWallDistance < 1e-10 {
 			s.LimitedWallDistance = 1e-10
 		}
 	}
+
+	if len(s.DNuHatDX) != s.NDim {
+		panic("nu deriv length mismatch")
+	}
+	if len(s.DUIdXJ) != s.NDim {
+		panic("velocity deriv length mismatch")
+	}
+	for _, v := range s.DUIdXJ {
+		if len(v) != s.NDim {
+			panic("velocity deriv length mismatch")
+		}
+	}
+
+	s.W = make([][]float64, s.NDim)
+	for i := range s.W {
+		s.W[i] = make([]float64, s.NDim)
+	}
 	s.Chi = s.NuHat / s.Nu
 	s.computeFt2()
 	s.computeSHat()
-	if !s.Unlimited {
-		if s.SHat < 1e-10 {
-			s.SHat = 1e-10
-		}
-	}
+
+	s.InvSHat = 1.0 / math.Max(s.SHat, 10e-10)
 	s.computeFw()
 	s.computeFv1()
 	s.computeProduction()
