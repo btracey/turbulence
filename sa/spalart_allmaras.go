90ee795a0f4dd08fd5d8fdd2b0ec7e70c36ec77f
btracey <bdtinternet@gmail.com>
2014-11-07 14:42:15 -0800

    added many functions to SA

--- sa/spalart_allmaras.go
+++ sa/spalart_allmaras.go
@@ -57,29 +57,29 @@ type SA struct {
 	MatDerivDNuDt float64
 }
 
-func (s *SA) computeFv1() {
+func (s *SA) ComputeFv1() {
 	chicubed := math.Pow(s.Chi, 3)
 	s.Fv1 = chicubed / (chicubed + CV1Cubed)
 }
 
-func (s *SA) computeFt2() {
+func (s *SA) ComputeFt2() {
 	s.Ft2 = CT3 * math.Exp(-CT4*s.Chi*s.Chi)
 }
 
-func (s *SA) computeFv2() {
-	s.computeFv1()
+func (s *SA) ComputeFv2() {
+	s.ComputeFv1()
 	s.Fv2 = 1 - s.Chi/(1+s.Chi*s.Fv1)
 }
 
-func (s *SA) computeSHat() {
-	s.computeOmega()
-	s.computeFv2()
+func (s *SA) ComputeSHat() {
+	s.ComputeOmega()
+	s.ComputeFv2()
 	d2 := s.WallDistance * s.LimitedWallDistance
 	invk2d2 := 1.0 / (K2 * d2)
 	s.SHat = s.Omega + s.NuHat*s.Fv2*invk2d2
 }
 
-func (s *SA) computeOmega() {
+func (s *SA) ComputeOmega() {
 	// First, compute W
 	for i := range s.W {
 		for j := range s.W[i] {
@@ -97,7 +97,7 @@ func (s *SA) computeOmega() {
 	s.Omega = math.Sqrt(s.Omega)
 }
 
-func (s *SA) computeR() {
+func (s *SA) ComputeR() {
 	r := s.NuHat * s.InvSHat / (Kappa * Kappa * s.LimitedWallDistance * s.LimitedWallDistance)
 	if r > 10 {
 		r = 10
@@ -105,28 +105,28 @@ func (s *SA) computeR() {
 	s.R = r
 }
 
-func (s *SA) computeG() {
-	s.computeR()
+func (s *SA) ComputeG() {
+	s.ComputeR()
 	s.G = s.R + CW2*(math.Pow(s.R, 6)-s.R)
 }
 
-func (s *SA) computeFw() {
-	s.computeG()
+func (s *SA) ComputeFw() {
+	s.ComputeG()
 	limiter := (1 + CW3Sixthed) / (math.Pow(s.G, 6) + CW3Sixthed)
 	s.Fw = s.G * math.Pow(limiter, 1.0/6.0)
 }
 
-func (s *SA) computeProduction() {
+func (s *SA) ComputeProduction() {
 	//s.Production = CB1 * (1 - s.Ft2) * s.SHat * s.NuHat
 	s.Production = CB1 * s.SHat * s.NuHat
 }
 
-func (s *SA) computeDestruction() {
+func (s *SA) ComputeDestruction() {
 	//s.Destruction = (CW1*s.Fw - Cb1OverKappaSquared*s.Ft2) * (s.NuHat / s.WallDistance) * (s.NuHat / s.WallDistance)
 	s.Destruction = CW1 * s.Fw * (s.NuHat / s.LimitedWallDistance) * (s.NuHat / s.LimitedWallDistance)
 }
 
-func (s *SA) computeCrossProduction() {
+func (s *SA) ComputeCrossProduction() {
 	s.CrossProduction = 0
 	for i := 0; i < s.NDim; i++ {
 		s.CrossProduction += CB2OverSigma * s.DNuHatDX[i] * s.DNuHatDX[i]
@@ -162,15 +162,205 @@ func (s *SA) Source() float64 {
 		s.W[i] = make([]float64, s.NDim)
 	}
 	s.Chi = s.NuHat / s.Nu
-	s.computeFt2()
-	s.computeSHat()
+	s.ComputeFt2()
+	s.ComputeSHat()
 
 	s.InvSHat = 1.0 / math.Max(s.SHat, 10e-10)
-	s.computeFw()
-	s.computeFv1()
-	s.computeProduction()
-	s.computeDestruction()
-	s.computeCrossProduction()
+	s.ComputeFw()
+	s.ComputeFv1()
+	s.ComputeProduction()
+	s.ComputeDestruction()
+	s.ComputeCrossProduction()
 	s.SourceTerm = s.Production - s.Destruction + s.CrossProduction
 	return s.SourceTerm
+}
+
+const (
+	//NuAir = 1.48e-5
+	NuAir = 1.0
+)
+
+/*
+func NuAlt(nu float64) float64 {
+	return NuAir
+}
+*/
+func NuAltScale(nu float64) float64 {
+	return nu / NuAir
+}
+
+func NuHatAlt(nu, nuhat float64) float64 {
+	nuAlt := NuAltScale(nu)
+	return nuhat / nuAlt
+}
+
+func NuHatAltStar(nu, nuhat float64) float64 {
+	nuHatAlt := NuHatAlt(nu, nuhat)
+	return nuHatAlt + 3*NuAir
+}
+
+func DistAlt(dist, nu float64) float64 {
+	//nuAlt := NuAltScale(nu)
+	//return dist / nuAlt
+	return dist
+}
+
+func DistAltStar(dist, nu float64) float64 {
+	//distAlt := DistAlt(dist, nu)
+	//return math.Min(distAlt, 300*NuAir) // 100 * nuhat_0
+
+	return math.Min(dist, 300*nu)
+}
+
+func SourceAlt(source, nu float64) float64 {
+	nutilde := NuAltScale(nu)
+	return source / (nutilde * nutilde)
+}
+
+func SourceNondimerAlt(nu, nuhat, dist float64) float64 {
+	nuhatAlt := NuHatAlt(nu, nuhat)
+	distAlt := DistAlt(dist, nu)
+	return (nuhatAlt / distAlt) * (nuhatAlt / distAlt)
+}
+
+func SourceNondimerAltStar(nu, nuhat, dist float64) float64 {
+	nuhatAltStar := NuHatAltStar(nu, nuhat)
+	distStar := DistAltStar(dist, nu)
+	sourceNondimer := (nuhatAltStar / distStar) * (nuhatAltStar / distStar)
+	/*
+		if sourceNondimer > 500000 && dist != 0 {
+			fmt.Println("nu", nu)
+			fmt.Println("nuhat", nuhat)
+			fmt.Println("dist", dist)
+			fmt.Println("nuhatalt", NuHatAlt(nu, nuhat))
+			fmt.Println("nu hat alt Star", nuhatAltStar)
+			fmt.Println(distStar)
+			fmt.Println(sourceNondimer)
+			os.Exit(1)
+		}
+	*/
+	return sourceNondimer
+}
+
+func InvSourceNondimerAltStar(nu, nuhat, dist float64) float64 {
+	return 1 / SourceNondimerAltStar(nu, nuhat, dist)
+}
+
+func NondimSourceAltStar(source, nu, nuhat, dist float64) float64 {
+	sourcenondimer := SourceNondimerAltStar(nu, nuhat, dist)
+	sourceAlt := SourceAlt(source, nu)
+	return sourceAlt / sourcenondimer
+}
+
+func NondimSourceAlt(source, nu, nuhat, dist float64) float64 {
+	sourcenondimer := SourceNondimerAlt(nu, nuhat, dist)
+	sourceAlt := SourceAlt(source, nu)
+	return sourceAlt / sourcenondimer
+}
+
+func OmegaAlt(omega, nu float64) float64 {
+	nutilde := NuAltScale(nu)
+	return omega / nutilde
+}
+
+func OmegaAltNondimRatio(nu, nuhat, dist float64) float64 {
+	nuhatAltStar := NuHatAltStar(nu, nuhat)
+	distStar := DistAltStar(dist, nu)
+	ratOrig := nuhat / (dist * dist)
+	ratNew := nuhatAltStar / (distStar * distStar)
+	return ratNew / ratOrig
+}
+
+func LogOmegaAltNondimRatio(nu, nuhat, dist float64) float64 {
+	nuhatAltStar := NuHatAltStar(nu, nuhat)
+	distStar := DistAltStar(dist, nu)
+	ratOrig := nuhat / (dist * dist)
+	ratNew := nuhatAltStar / (distStar * distStar)
+	return math.Log(ratNew) - math.Log(ratOrig)
+}
+
+func SourceAltNondimRatio(nu, nuhat, dist float64) float64 {
+	nuhatAltStar := NuHatAltStar(nu, nuhat)
+	distStar := DistAltStar(dist, nu)
+	ratOrig := (nuhat / dist) * (nuhat / dist)
+	ratNew := (nuhatAltStar / distStar) * (nuhatAltStar / distStar)
+	return ratNew / ratOrig
+}
+
+func LogSourceAltNondimRatio(nu, nuhat, dist float64) float64 {
+	nuhatAltStar := NuHatAltStar(nu, nuhat)
+	distStar := DistAltStar(dist, nu)
+	ratOrig := (nuhat / dist) * (nuhat / dist)
+	ratNew := (nuhatAltStar / distStar) * (nuhatAltStar / distStar)
+	return math.Log(ratNew) - math.Log(ratOrig)
+}
+
+func OmegaNondimerAlt(nu, nuhat, dist float64) float64 {
+	nuhatAlt := NuHatAlt(nu, nuhat)
+	distAlt := DistAlt(dist, nu)
+	return (1 / distAlt) * (nuhatAlt / distAlt)
+}
+
+func OmegaNondimerAltStar(nu, nuhat, dist float64) float64 {
+	nuhatAltStar := NuHatAltStar(nu, nuhat)
+	distStar := DistAltStar(dist, nu)
+	return (1 / distStar) * (nuhatAltStar / distStar)
+}
+
+func InvOmegaNondimerAltStar(nu, nuhat, dist float64) float64 {
+	return 1 / OmegaNondimerAltStar(nu, nuhat, dist)
+}
+
+func NondimOmegaAlt(omega, nu, nuhat, dist float64) float64 {
+	omegaNondimer := OmegaNondimerAlt(nu, nuhat, dist)
+	omegaAlt := OmegaAlt(omega, nu)
+	return omegaAlt / omegaNondimer
+}
+
+func NondimOmegaAltStar(omega, nu, nuhat, dist float64) float64 {
+	omegaNondimer := OmegaNondimerAltStar(nu, nuhat, dist)
+	omegaAlt := OmegaAlt(omega, nu)
+	return omegaAlt / omegaNondimer
+}
+
+func ChiAlt(chi float64) float64 {
+	return math.Min(chi, 100)
+}
+
+func NuGradMagAlt(nuGradMagAlt, nu float64) float64 {
+	nutilde := NuAltScale(nu)
+	return nuGradMagAlt / (nutilde * nutilde)
+}
+
+func NondimNuGradMagAlt(grad, nu, nuhat, dist float64) float64 {
+	sourcenondimer := SourceNondimerAlt(nu, nuhat, dist)
+	nuGradAlt := NuGradMagAlt(grad, nu)
+	return nuGradAlt / sourcenondimer
+}
+
+func NondimNuGradMagAltStar(grad, nu, nuhat, dist float64) float64 {
+	sourcenondimer := SourceNondimerAltStar(nu, nuhat, dist)
+	nuGradAlt := NuGradMagAlt(grad, nu)
+
+	/*
+		fmt.Println(nuGradAlt)
+
+		if nuGradAlt/sourcenondimer > 170 {
+			fmt.Println()
+			fmt.Println("dist", dist)
+			fmt.Println("dist alt", DistAlt(dist, nu))
+			fmt.Println("dist alt *", DistAltStar(dist, nu))
+			fmt.Println("nu", nu)
+			fmt.Println("nuscale", NuAltScale(nu))
+			fmt.Println("nuhat", nuhat)
+			fmt.Println("nuhat alt *", NuHatAlt(nu, nuhat))
+			fmt.Println("nuhat alt star ", NuHatAltStar(nu, nuhat))
+			fmt.Println(sourcenondimer)
+			fmt.Println(grad)
+			fmt.Println(nuGradAlt)
+			fmt.Println(nuGradAlt / sourcenondimer)
+			os.Exit(1)
+		}
+	*/
+	return nuGradAlt / sourcenondimer
 }
